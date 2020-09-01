7086b8683db3dfa163d6b353c6c4c92c39511cd8
Wojciech Rogóż <wrogoz@gmail.com>
2020-09-01 13:06:48 +0200

    basic settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
+import {Switch, Route, Link,NavLink} from 'react-router-dom';
+import Home from './components/home'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Route path='/'>
+        <Home/>
+      </Route>
+    </Container>
   );
 }
 
+
+
+const Container = styled.div`
+  width:100vw;
+  height:100vh;
+  background:pink;
+`
 export default App;
