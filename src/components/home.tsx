fb90b39112ee5e8318926c593a74d6f2bd45712f
Wojciech Rogóż <wrogoz@gmail.com>
2020-09-01 14:25:11 +0200

    home component in progress

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,12 +1,61 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import bgNoise from '../images/bgNoise.gif'
 const Home = ()=>{
-    return(<div>
-         elo world
-        </div>
+    return(<HomeBackground>
+        <HomeContainer>
+
         
+          <Header>
+          
+                  nawigacja
+           
+          </Header>
+          <NameSection>
+            <h1>
+              <span>Rogóż</span>  
+              <span>Wojciech</span>  
+            </h1>
+          </NameSection>
+
+       
+        </HomeContainer>
+        </HomeBackground>
     )
 }
+ const HomeContainer = styled.div`
+ display:flex;
+ flex-direction:column;
+    height:100%;
+    width:100%;
+    background-color:rgba(0,0,0,0.9);
 
+ `
+const HomeBackground = styled.div`
+    height:100%;
+    width:100%;
+background-image:url("${bgNoise}")
+`
+ const Header = styled.header`
+    height:70%;
+    width:100%;
+    color:#fff;
+      
+ 
+    
+  
+ `
+ const NameSection =  styled.section`
+    width:80%;
+    height:30%;
+    color:#fff;
+    
+    text-transform:uppercase;
+    h1{
+        display:flex;
+        flex-direction:column;
+        margin-left:20px;
+    }
+    
+ `
 export default Home
