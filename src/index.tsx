334871c2c54982f456742527f486a24bce123b5f
Wojciech Rogoz <wrogoz@gmail.com>
2020-09-02 08:20:30 +0200

    basic styles added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import './basicStyle.css';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom'
 ReactDOM.render(
